a6d775c4dfda44cb182ea7d5ce6ff8e23d50c38a
Volk-QA <kriusovia@gmail.com>
2019-12-06 18:24:26 +0300

    Added test marking for Allure

--- test_main_page.py
+++ test_main_page.py
@@ -2,8 +2,10 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
+import allure
 link = "http://selenium1py.pythonanywhere.com/"
 
+@allure.feature('critical')
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
 	def test_guest_can_go_to_login_page(self, browser): 
@@ -16,6 +18,8 @@ class TestLoginFromMainPage():
 		page = MainPage(browser, link)
 		page.open()
 		page.should_be_login_link()
+
+@allure.feature('critical')
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
 	page = MainPage(browser, link)
 	page.open()
--- test_product_page.py
+++ test_product_page.py
@@ -3,11 +3,13 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
 import time
+import allure
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 reg_link ="http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
-@pytest.mark.need_review
+@allure.feature('critical')
+@allure.feature('Parameterize')
 @pytest.mark.parametrize('offer', [0, 1, 2, 3, 4, 5, 6,
                                   pytest.param(7, marks=pytest.mark.xfail),
                                   8, 9])
@@ -19,6 +21,8 @@ def test_guest_can_add_product_to_basket(browser, offer):
 	page.solve_quiz_and_get_code()
 	page.check_add_to_basket_true()
 
+@allure.feature('low')
+@allure.feature('Xfail')
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 	page = ProductPage(browser, link)
@@ -26,11 +30,14 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 	page.add_to_basket()
 	page.should_not_be_success_message()
 
+@allure.feature('low')
 def test_guest_cant_see_success_message(browser):
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_not_be_success_message()
 
+@allure.feature('low')
+@allure.feature('Xfail')
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
 	page = ProductPage(browser, link)
@@ -38,13 +45,14 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 	page.add_to_basket()
 	page.should_element_is_disappeared()
 
+@allure.feature('critical')
 def test_guest_should_see_login_link_on_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_be_login_link()
 
-@pytest.mark.need_review
+@allure.feature('critical')
 def test_guest_can_go_to_login_page_from_product_page(browser):
 	page = ProductPage(browser, link)
 	page.open()
@@ -52,7 +60,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 	login_page = LoginPage(browser, browser.current_url)
 	login_page.should_be_login_page()
 
-@pytest.mark.need_review
+@allure.feature('critical')
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	page = ProductPage(browser, link)
 	page.open()
@@ -60,6 +68,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	page = BasketPage(browser, browser.current_url)
 	page.check_basket()
 
+@allure.feature('critical')
 @pytest.mark.user_add_to_basket
 class TestUserAddToBasketFromProductPage():
 	@pytest.fixture(scope="function", autouse=True)
@@ -77,7 +86,6 @@ class TestUserAddToBasketFromProductPage():
 		page.should_be_authorized_user()
 		page.should_not_be_success_message()
 
-	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
 		page = ProductPage(browser, link)
 		page.open()

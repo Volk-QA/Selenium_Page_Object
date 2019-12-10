b9c033192d3971b25c47feecd854710dbb9b7813
Volk-QA <kriusovia@gmail.com>
2019-12-10 22:53:54 +0300

    Delete selenoid endpoint

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Automated Tests for online shop:
 http://selenium1py.pythonanywhere.com/ru/catalogue/
 
-Language: Python(Selenoid + Selenium + Pytest + Allure)
+Language: Python(Selenium + Pytest + Allure)
 Design pattern in tests: PageObject
 
--- conftest.py
+++ conftest.py
@@ -2,14 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-capabilities = {
-	"browserName": "chrome",
-	"version": "78.0",
-	"enableVNC": True,
-	"enableVideo": True
-}
-
-
 def pytest_addoption(parser):
 	parser.addoption('--language', action='store', default='en', help='Choose language')
 
@@ -20,11 +12,7 @@ def browser(request):
 	options = Options()
 	options.add_experimental_option('prefs', {'intl.accept_languages': lang})
 	print('\nstart Chrome browser...')
-	browser = webdriver.Remote(
-	command_executor="http://localhost:4444/wd/hub",
-	desired_capabilities=capabilities,
-	options=options
-	)
+	browser = webdriver.Chrome(options=options)
 	yield browser
 	print('\nquit Chrome browser...')
 	browser.quit()
